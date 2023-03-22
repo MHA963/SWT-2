1169afad40ded7cbb22144a7d888882fc602d172
Hashem Abdou <MHA_SW@users.noreply.github.com>
2023-03-22 12:20:12 +0100

    add .

---
+++ ChargingStation/ChargingStationApp/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+        static void Main(string[] args)
+        {
+				// Assemble your system here from all the classes
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        Door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        Door.OnDoorClose();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
+        }
+}
