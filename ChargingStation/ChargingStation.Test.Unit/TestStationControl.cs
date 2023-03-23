de2ddc1cdd6e90909661a448d5ac85a4cfc5b497
Hashem Abdou <MHA_SW@users.noreply.github.com>
2023-03-23 15:30:28 +0100

    Adding StationControl.test

--- ChargingStation/ChargingStation.Test.Unit/TestStationControl.cs
+++ ChargingStation/ChargingStation.Test.Unit/TestStationControl.cs
@@ -56,5 +56,104 @@ namespace ChargingStation.Test.Unit
         }
 
 
+        [Test]
+        public void TestRfidDetected_locked_iswrong()
+        {
+            _uut._state = Locked;
+            _uut._oldId = 456;
+            _uut.RfidDetected(this,new RfidReader(){Id = 12345});
+            _uut._display.RFIDFejl();
+            Assert.That(_uut._state,Is.EqualTo(Locked));
+
+        }
+
+        [Test]
+        public void TestRfidDetected_DoorOpen()
+        {
+            _uut._state = DoorOpen;
+            _uut._oldId = 12345;
+            _uut.RfidDetected(this, new RfidReader(){Id=12345});
+            Assert.That(_uut._state, Is.EqualTo(DoorOpen));
+        }
+
+        [Test]
+        public void TestRfidDetected_PhoneConnected()
+        {
+            RfidReader rfidReader = new RfidReader() { Id = 12345 };
+            _uut._charger.IsConnected = true;
+            _uut._state = Available;
+            _uut.RfidDetected(this, rfidReader);
+            _uut._door.Received(1).LockDoor();
+            _uut._charger.Received(1).StartCharge();
+            _uut._log.Received(1).WriteLogEntry("Skab er låst med RFID", rfidReader.Id);
+            _uut._display.Received(1).LadeskabOptaget();
+            _uut._display.Received(1).TilslutTelefon();
+            Assert.That(_uut._state, Is.EqualTo(Locked));
+        }
+
+        [Test]
+        public void TestRfidDetected_PhoneNotConnected()
+        {
+            _uut._charger.IsConnected = false;
+            _uut._state = Available;
+            _uut.RfidDetected(this, new RfidReader(){Id=12345});
+            _uut._display.Received(1).TilslutTelefon();
+            Assert.That(_uut._state,Is.EqualTo(Available));
+
+        }
+
+        [Test]
+
+        public void TestRfidDetected_PhoneConnected_DoorOpen()
+        {
+            _uut._charger.IsConnected = true;
+            _uut._state = DoorOpen;
+            _uut.RfidDetected(this, new RfidReader() { Id=12345});
+            _uut._display.Received(1).TelefonTilsluttet();
+            Assert.That(_uut._state,Is.EqualTo(DoorOpen));
+        }
+
+        [Test]
+
+        public void TestRfidDetected_PhoneConnected_Locked()
+        {
+            _uut._charger.IsConnected = true;
+            _uut._state = Locked;
+            _uut.RfidDetected(this, new RfidReader() { Id=12345});
+            _uut._display.Received(1).TelefonTilsluttet();
+            Assert.That(_uut._state,Is.EqualTo(Locked));
+        }
+
+        [Test]
+
+        public void TestRfidDetected_PhoneConnected_Locked_iswrong()
+        {
+            _uut._charger.IsConnected = true;
+            _uut._state = Locked;
+            _uut.RfidDetected(this, new RfidReader() { Id=123456});
+            _uut._display.Received(1).RFIDFejl();
+            Assert.That(_uut._state,Is.EqualTo(Locked));
+        }
+
+        [Test]
+
+        public void TestDoorClosedStateAvaialble()
+        {
+            _uut._state = Available;
+            _uut.DoorLocked(this,new DoorEventArgs(){DoorIsOpen = false});
+            Assert.That(_uut._state,Is.EqualTo(Available));
+
+        }
+
+        [Test]
+        public void TestDoorClosedStateLocked()
+        {
+            _uut._state = Locked;
+            _uut.DoorLocked(this,new DoorEventArgs(){DoorIsOpen = false});
+            Assert.That(_uut._state,Is.EqualTo(Locked));
+
+        }
+
+
     }
 }
