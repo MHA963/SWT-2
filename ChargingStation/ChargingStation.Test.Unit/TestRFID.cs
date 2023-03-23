d0c17f0249982a5ee7ef81fd81c40949090ea13d
JeppePape <83404786+JeppePape@users.noreply.github.com>
2023-03-23 15:40:06 +0100

    Finished RFID, USB Tests

---
+++ ChargingStation/ChargingStation.Test.Unit/TestRFID.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargingStation.lib.Simulators;
+using ChargingStation.lib.Interfaces;
+
+namespace ChargingStation.Test.Unit
+{
+    internal class TestRFID
+    {
+        private RFidReader rfid;
+        private int Id;
+
+        [SetUp]
+        public void Setup()
+        {
+            rfid = new RFidReader();
+            rfid.RfidEvent += RfidDetected;
+        }
+
+        private void RfidDetected(object source, RfidReader e)
+        {
+            Id = e.Id;
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(-100)]
+        public void TestRFIDDetected(int id)
+        {
+            rfid.RfidDetected(id);
+            Assert.That(Id, Is.EqualTo(id));
+        }
+    }
+}
